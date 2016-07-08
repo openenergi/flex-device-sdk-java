de15b2f7e5dfc1e966fa03065cebd0fa152bf251
Michael Bironneau <michael.bironneau@openenergi.com>
2016-07-08 11:06:22 +0100

    Fix failing test

--- src/test/java/test/com/openenergi/flex/message/SignalTest.java
+++ src/test/java/test/com/openenergi/flex/message/SignalTest.java
@@ -7,17 +7,14 @@ import java.time.LocalDateTime;
 
 import org.json.JSONException;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 
 public class SignalTest {
 	
 
-	@Ignore
 	@Test
 	public void testSerializeEvent() {
-		@SuppressWarnings("unchecked")
 		Signal<SignalPointItem> s = (Signal<SignalPointItem>) new Signal<SignalPointItem>()
 					.addEntity("l1")
 					.setTimestamp(12345L)
@@ -26,8 +23,9 @@ public class SignalTest {
 		s.addItem(new SignalPointItem(LocalDateTime.of(2016,12,27, 0, 0, 0, 0), 1.23));
 		
 		try {
-			JSONAssert.assertEquals("\"topic\": \"signals\", \"entities\":[\"l1\"],\"items\":[{\"start_at\":1482796800000,\"value\":1.23}],\"timestamp\":12345,\"type\":\"something\"}", s.toString(), true);
-		} catch (JSONException e1) {
+            JSONAssert.assertEquals("{\"topic\": \"signals\", \"entities\":[\"l1\"],\"items\":[{\"start_at\":1482796800000,\"value\":1.23}],\"timestamp\":12345,\"type\":\"something\"}", s.toString(), true);
+
+        } catch (JSONException e1) {
 			fail("Failed to serialize: " + e1.getMessage());
 		}
 	}
@@ -37,7 +35,6 @@ public class SignalTest {
 		String s = "{\"topic\": \"signals\", \"entities\":[\"l1\"],\"items\":[{\"start_at\":1482796800000,\"value\":1.23}],\"timestamp\":12345,\"type\":\"something\"}";
 
 			Signal<SignalPointItem> m = (Signal<SignalPointItem>) Message.deserialize(s);
-			//System.out.println(m.toString());
 			assertEquals(m.entities.get(0), "l1");
 			assertEquals(m.topic, "signals");
 			assertEquals((Long) 12345L, m.timestamp);
